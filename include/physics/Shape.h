cca7bc8d5c63383852a9677451b4ff098ea8ce0b
jgoo <maruluv0527@gmail.com>
2024-11-27 17:58:28 +0900

    [Feat]: Created Shape.h

---
+++ include/physics/Shape.h
@@ -0,0 +1,30 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <glm/glm.hpp>
+
+struct MassData
+{
+    glm::vec3 center;
+    float mass;
+    // inertia
+    float I;
+};
+
+class Shape
+{
+public:
+    enum class Type
+    {
+        e_sphere = 0,
+        e_box = 1
+    };
+
+    virtual ~Shape();
+    virtual Shape* clone() const = 0;
+    virtual int GetChildCount() const = 0;
+    virtual void ComputeAABB() const = 0;
+    virtual void ComputeMass() const= 0;
+};
+
+#endif
