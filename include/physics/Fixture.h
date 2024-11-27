012f480afa63d74bdbb291a90b94a15715fc220e
jgoo <maruluv0527@gmail.com>
2024-11-27 16:27:34 +0900

    [Feat] : Created Fixture.h

---
+++ include/physics/Fixture.h
@@ -0,0 +1,57 @@
+#ifndef FIXTURE_H
+#define FIXTURE_H
+
+#include <glm/glm.hpp>
+
+class Shape;
+class BroadPhase;
+class Rigidbody;
+
+struct FixtureDef
+{
+    FixtureDef()
+    {
+        shape = nullptr;
+        userData = nullptr;
+        friction = 0.2f;
+        restitution = 0.0f;
+        density = 0.0f;
+    }
+    const Shape* shape;
+    void* userData;
+    float friction;
+    float restitution;
+    float density;
+};
+
+struct FixtureProxy
+{
+    AABB aabb;
+    Fixture* fixture;
+    int childIndex;
+    int proxyId;
+};
+
+class Fixture
+{
+public:
+    void Create(FixtureDef* def);
+    void Destroy();
+
+    void CreateProxies(BroadPhase* broadPhase, const Transform& xf);
+    void DestroyProxies(BroadPhase* broadPhase);
+
+protected:
+    Rigidbody*  body;
+    Shape*      shape;
+    float       density;
+    float       friction;
+    float       restitution;
+    // std::vector<FixtureProxy> proxies;
+    void*       userData;
+
+private:
+
+};
+
+#endif
