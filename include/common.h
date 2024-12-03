4bdab37aa66e2de90a00d58b3429320f2ae55bef
jgoo <maruluv0527@gmail.com>
2024-12-03 21:37:45 +0900

    [Feat]: glm quaternion header added

--- include/common.h
+++ include/common.h
@@ -5,7 +5,9 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
