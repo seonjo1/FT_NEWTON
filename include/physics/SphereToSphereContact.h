a088d4b65ec9d43d413d465c44147152c98c8ffd
seonjo1 <tjdgus9684@gmail.com>
2024-12-11 01:54:53 +0900

    [Feat]: add evaluate in SphereToSphereContact.h

--- include/physics/SphereToSphereContact.h
+++ include/physics/SphereToSphereContact.h
@@ -10,7 +10,8 @@ class SphereToSphereContact : public Contact
   public:
 	static Contact *create(Fixture *fixtureA, Fixture *fixtureB, int32_t indexA, int32_t indexB);
 	SphereToSphereContact(Fixture *fixtureA, Fixture *fixtureB, int32_t indexA, int32_t indexB);
-}
+	virtual void evaluate(Manifold &manifold, const Transform &transformA, const Transform &transformB) override;
+};
 } // namespace ale
 
 #endif
