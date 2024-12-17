de73a4dd70e31870f56fd081f27232acf071ec38
jgoo <maruluv0527@gmail.com>
2024-12-17 18:54:44 +0900

    [Fix]: m_center fixed to xf!

--- src/physics/SphereShape.cpp
+++ src/physics/SphereShape.cpp
@@ -22,8 +22,8 @@ int32_t SphereShape::getChildCount() const
 void SphereShape::computeAABB(AABB *aabb, const Transform &xf) const
 {
 	// get min, max vertex
-	glm::vec3 upper = m_center + glm::vec3(m_radius);
-	glm::vec3 lower = m_center - glm::vec3(m_radius);
+	glm::vec3 upper = xf.position + glm::vec3(m_radius);
+	glm::vec3 lower = xf.position - glm::vec3(m_radius);
 
 	// std::cout << "upper: " << upper.x << ", " << upper.y << ", " << upper.z << '\n';
 	// std::cout << "lower: " << lower.x << ", " << lower.y << ", " << lower.z << '\n';
