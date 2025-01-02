ce6e5684c8927a3a90d621ae56bcf5b9c2bae312
seonjo1 <tjdgus9684@gmail.com>
2025-01-03 00:33:09 +0900

    [Fix]: Change implementation to generate contacts using GJK + EPA (SphereToSphereContact)

--- include/physics/SphereToSphereContact.h
+++ include/physics/SphereToSphereContact.h
@@ -10,7 +10,11 @@ class SphereToSphereContact : public Contact
   public:
 	static Contact *create(Fixture *fixtureA, Fixture *fixtureB, int32_t indexA, int32_t indexB);
 	SphereToSphereContact(Fixture *fixtureA, Fixture *fixtureB, int32_t indexA, int32_t indexB);
-	virtual void evaluate(Manifold &manifold, const Transform &transformA, const Transform &transformB) override;
+
+	virtual glm::vec3 supportA(const ConvexInfo &sphere, glm::vec3 dir) override;
+	virtual glm::vec3 supportB(const ConvexInfo &sphere, glm::vec3 dir) override;
+	virtual void findCollisionPoints(const ConvexInfo &sphereA, const ConvexInfo &sphereB, std::vector<CollisionInfo> &collisionInfoVector,
+							 EpaInfo &epaInfo, std::vector<Simplex> &simplexVector) override;
 };
 } // namespace ale
 
--- src/physics/SphereToSphereContact.cpp
+++ src/physics/SphereToSphereContact.cpp
@@ -11,50 +11,27 @@ Contact *SphereToSphereContact::create(Fixture *fixtureA, Fixture *fixtureB, int
 	return new SphereToSphereContact(fixtureA, fixtureB, indexA, indexB);
 }
 
-void SphereToSphereContact::evaluate(Manifold &manifold, const Transform &transformA, const Transform &transformB)
+glm::vec3 SphereToSphereContact::supportA(const ConvexInfo &sphere, glm::vec3 dir)
 {
-	/*
-		64bit = 27bit(small proxyId) 	 |
-				27bit(big proxyId) 		 |
-				5bit(small contact part) |
-				5bit(big contact part)
-	*/
-	static int64_t bitmask = 0xFFFFFFFF & ~0b11111;
-
-	ManifoldPoint manifoldPoint;
-	manifoldPoint.type = EManifoldType::FACE_A_TO_FACE_B;
-
-	Shape *shapeA = m_fixtureA->getShape();
-	Shape *shapeB = m_fixtureB->getShape();
-
-	int64_t proxyIdA = m_fixtureA->getFixtureProxy()->proxyId;
-	int64_t proxyIdB = m_fixtureB->getFixtureProxy()->proxyId;
-
-	if (proxyIdA > proxyIdB)
-	{
-		int64_t tmp = proxyIdA;
-		proxyIdA = proxyIdB;
-		proxyIdB = tmp;
-	}
-
-	proxyIdA = (proxyIdA << 5) & bitmask;
-	proxyIdB = (proxyIdB << 5) & bitmask;
-	manifoldPoint.id = (proxyIdA << 32) | (proxyIdB << 10);
-
-	glm::vec3 worldCenterA = transformA.toMatrix() * glm::vec4(shapeA->localCenter, 1.0f);
-	glm::vec3 worldCenterB = transformB.toMatrix() * glm::vec4(shapeB->localCenter, 1.0f);
-
-	manifoldPoint.normal = glm::normalize(worldCenterB - worldCenterA);
-	manifoldPoint.pointA = worldCenterA + shapeA->getLocalRadius() * manifoldPoint.normal;
-	manifoldPoint.pointB = worldCenterB - shapeB->getLocalRadius() * manifoldPoint.normal;
-	manifoldPoint.seperation = glm::length(manifoldPoint.pointA - manifoldPoint.pointB);
-
-
-	if (glm::length2(manifoldPoint.pointA - worldCenterB) < shapeB->getLocalRadius() * shapeB->getLocalRadius() &&
-		glm::length2(manifoldPoint.pointB - worldCenterA) < shapeA->getLocalRadius() * shapeA->getLocalRadius())
-	{
-		manifold.points.push_back(manifoldPoint);
-	}
+	return sphere.center + dir * sphere.radius;
 }
 
+glm::vec3 SphereToSphereContact::supportB(const ConvexInfo &sphere, glm::vec3 dir)
+{
+	return sphere.center + dir * sphere.radius;
+}
+
+void SphereToSphereContact::findCollisionPoints(const ConvexInfo &sphereA, const ConvexInfo &sphereB,
+												std::vector<CollisionInfo> &collisionInfoVector, EpaInfo &epaInfo,
+												std::vector<Simplex> &simplexVector)
+{
+	CollisionInfo collisionInfo;
+
+	collisionInfo.normal = epaInfo.normal;
+	collisionInfo.seperation = epaInfo.distance;
+	collisionInfo.pointA = sphereA.center + epaInfo.normal * sphereA.radius;
+	collisionInfo.pointB = collisionInfo.pointA - collisionInfo.normal * collisionInfo.seperation; 
+	
+	collisionInfoVector.push_back(collisionInfo);
+}
 } // namespace ale
