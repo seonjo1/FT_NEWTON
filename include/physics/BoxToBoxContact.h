435b44bb2caa85911938e7c567a56d3156fbfb06
seonjo1 <tjdgus9684@gmail.com>
2025-01-09 14:24:13 +0900

    [Fix]: move functions from BoxToBoxContact to Contact

--- include/physics/BoxToBoxContact.h
+++ include/physics/BoxToBoxContact.h
@@ -16,15 +16,6 @@ class BoxToBoxContact : public Contact
 	virtual glm::vec3 supportB(const ConvexInfo &box, glm::vec3 dir) override;
 	virtual void findCollisionPoints(const ConvexInfo &boxA, const ConvexInfo &boxB, std::vector<CollisionInfo> &collisionInfoVector,
 							 EpaInfo &epaInfo, std::vector<Simplex> &simplexVector) override;
-	
-	Face getPolygonFace(const ConvexInfo &box, const glm::vec3 &normal);
-	std::vector<glm::vec3> computeContactPolygon(const Face &refFace, const Face &incFace);
-	std::vector<glm::vec3> clipPolygonAgainstPlane(const std::vector<glm::vec3> &polygon, const glm::vec3 &planeNormal,
-												   float planeDist);
-
-	void buildManifoldFromPolygon(std::vector<CollisionInfo> &collisionInfoVector, const Face &refFace,
-								  const Face &incFace, std::vector<glm::vec3> &polygon, EpaInfo &epaInfo);
-	void sortPointsClockwise(std::vector<glm::vec3> &points, const glm::vec3 &center, const glm::vec3 &normal);
 };
 } // namespace ale
 
