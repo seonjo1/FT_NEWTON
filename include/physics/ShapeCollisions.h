6f02048a338a7c22357c4c5f977a3e476feba2c4
seonjo1 <tjdgus9684@gmail.com>
2025-01-10 15:02:40 +0900

    [Feat]: add ShapeCollisions.h

---
+++ include/physics/ShapeCollisions.h
@@ -0,0 +1,19 @@
+#ifndef SHAPECOLLISIONS_H
+#define SHAPECOLLISIONS_H
+
+#include "physics/SphereToSphereContact.h"
+#include "physics/SphereToBoxContact.h"
+#include "physics/SphereToCylinderContact.h"
+#include "physics/SphereToCapsuleContact.h"
+
+#include "physics/BoxToBoxContact.h"
+#include "physics/BoxToCylinderContact.h"
+#include "physics/BoxToCapsuleContact.h"
+
+#include "physics/CylinderToCylinderContact.h"
+#include "physics/CylinderToCapsuleContact.h"
+
+#include "physics/CapsuleToCapsuleContact.h"
+
+
+#endif
