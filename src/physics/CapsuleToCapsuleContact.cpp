9360ed8c8423bcbfc557f3ca7c4a37c463d4f52f
seonjo1 <tjdgus9684@gmail.com>
2025-01-11 16:39:46 +0900

    [Feat]: implement CapsuleToCapsuleContact

--- src/physics/CapsuleToCapsuleContact.cpp
+++ src/physics/CapsuleToCapsuleContact.cpp
@@ -13,19 +13,81 @@ Contact *CapsuleToCapsuleContact::create(Fixture *fixtureA, Fixture *fixtureB, i
 
 glm::vec3 CapsuleToCapsuleContact::supportA(const ConvexInfo &capsule, glm::vec3 dir)
 {
-
+	float dotResult = glm::dot(dir, capsule.axes[0]);
+	if (dotResult > 0)
+	{
+		return capsule.points[0] + dir * capsule.radius;
+	}
+	else
+	{
+		return capsule.points[1] + dir * capsule.radius;
+	}
 }
 
 glm::vec3 CapsuleToCapsuleContact::supportB(const ConvexInfo &capsule, glm::vec3 dir)
 {
-
+	float dotResult = glm::dot(dir, capsule.axes[0]);
+	if (dotResult > 0)
+	{
+		return capsule.points[0] + dir * capsule.radius;
+	}
+	else
+	{
+		return capsule.points[1] + dir * capsule.radius;
+	}
 }
 
 void CapsuleToCapsuleContact::findCollisionPoints(const ConvexInfo &capsuleA, const ConvexInfo &capsuleB,
 												  std::vector<CollisionInfo> &collisionInfoVector, EpaInfo &epaInfo,
 												  std::vector<Simplex> &simplexVector)
 {
+	// clipping
+	if (isCollideToHemisphere(capsuleA, epaInfo.normal))
+	{
+		CollisionInfo collisionInfo;
 
+		collisionInfo.normal = epaInfo.normal;
+		collisionInfo.seperation = epaInfo.distance;
+		if (glm::dot(capsuleA.axes[0], epaInfo.normal) > 0)
+		{
+			collisionInfo.pointA = capsuleA.points[0] + epaInfo.normal * capsuleA.radius;
+			collisionInfo.pointB = collisionInfo.pointA - collisionInfo.normal * collisionInfo.seperation;
+		}
+		else
+		{
+			collisionInfo.pointA = capsuleA.points[1] + epaInfo.normal * capsuleA.radius;
+			collisionInfo.pointB = collisionInfo.pointA - collisionInfo.normal * collisionInfo.seperation;
+		}
+		
+		collisionInfoVector.push_back(collisionInfo);
+	}
+	else if (isCollideToHemisphere(capsuleB, -epaInfo.normal))
+	{
+		CollisionInfo collisionInfo;
+
+		collisionInfo.normal = epaInfo.normal;
+		collisionInfo.seperation = epaInfo.distance;
+		if (glm::dot(capsuleB.axes[0], collisionInfo.normal) < 0)
+		{
+			collisionInfo.pointB = capsuleB.points[0] + collisionInfo.normal * capsuleB.radius;
+			collisionInfo.pointA = collisionInfo.pointB - collisionInfo.normal * collisionInfo.seperation;
+		}
+		else
+		{
+			collisionInfo.pointB = capsuleB.points[1] + collisionInfo.normal * capsuleB.radius;
+			collisionInfo.pointA = collisionInfo.pointB - collisionInfo.normal * collisionInfo.seperation;
+		}
+		
+		collisionInfoVector.push_back(collisionInfo);
+	}
+	else
+	{
+		Face refFace = getCapsuleFace(capsuleA, epaInfo.normal);
+		Face incFace = getCapsuleFace(capsuleB, -epaInfo.normal);
+
+		std::vector<glm::vec3> contactPolygon = computeContactPolygon(refFace, incFace);
+		buildManifoldFromPolygon(collisionInfoVector, refFace, incFace, contactPolygon, epaInfo);
+	}
 }
 
 } // namespace ale
