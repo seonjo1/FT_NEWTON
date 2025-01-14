ec69a076e32b37495d3dba3b69a840e9485272e0
seonjo1 <tjdgus9684@gmail.com>
2025-01-14 18:31:40 +0900

    [Fix]: change cylinder support function in CylinderToCylinderContact

--- src/physics/CylinderToCylinderContact.cpp
+++ src/physics/CylinderToCylinderContact.cpp
@@ -3,7 +3,8 @@
 namespace ale
 {
 
-CylinderToCylinderContact::CylinderToCylinderContact(Fixture *fixtureA, Fixture *fixtureB, int32_t indexA, int32_t indexB)
+CylinderToCylinderContact::CylinderToCylinderContact(Fixture *fixtureA, Fixture *fixtureB, int32_t indexA,
+													 int32_t indexB)
 	: Contact(fixtureA, fixtureB, indexA, indexB) {};
 
 Contact *CylinderToCylinderContact::create(Fixture *fixtureA, Fixture *fixtureB, int32_t indexA, int32_t indexB)
@@ -17,33 +18,55 @@ glm::vec3 CylinderToCylinderContact::supportA(const ConvexInfo &cylinder, glm::v
 	glm::vec3 center = cylinder.center;				   // 중심점
 	glm::vec3 axis = glm::normalize(cylinder.axes[0]); // 높이 축 (단위 벡터)
 	float height = cylinder.height;
-	float radius = cylinder.radius;
 
 	// 1. 축 방향으로 윗면/아랫면 선택
-	glm::vec3 base = center;
 
-	if (glm::dot(dir, axis) > 0.0f)
+	bool isUpSide = true;
+	float dotResult = glm::dot(dir, axis);
+
+	if (dotResult < 0)
 	{
-		base += 0.5f * height * axis;
+		isUpSide = false;
+	}
+
+	glm::vec3 circleDir = dir - glm::dot(dir, axis) * axis; // 축에 수직한 방향
+
+	if (glm::length2(circleDir) > 1e-8f)
+	{
+		circleDir = glm::normalize(circleDir); // 정규화
+		
+		int32_t maxIdx;
+		int32_t segments = 20;
+
+		float max = -FLT_MAX;
+		for (int32_t i = 0; i < segments; ++i)
+		{
+			dotResult = glm::dot(cylinder.points[i], dir);
+			if (dotResult > max)
+			{
+				maxIdx = i;
+				max = dotResult;
+			}
+		}
+
+		if (isUpSide == false)
+		{
+			maxIdx += segments;
+		}
+
+		return cylinder.points[maxIdx];
 	}
 	else
 	{
-		base += -0.5f * height * axis;
+		if (isUpSide)
+		{
+			return center + cylinder.axes[0] * height * 0.5f;
+		}
+		else
+		{
+			return center - cylinder.axes[0] * height * 0.5f;
+		}
 	}
-
-	// 2. 원형 단면의 가장 먼 점 계산
-	glm::vec3 dirCircle = dir - glm::dot(dir, axis) * axis; // 축에 수직한 방향
-	if (glm::length(dirCircle) > 0.0001f)
-	{
-		dirCircle = glm::normalize(dirCircle); // 정규화
-	}
-	else
-	{
-		dirCircle = glm::vec3(0.0f, 0.0f, 0.0f); // 축 방향으로만 정렬된 경우
-	}
-	glm::vec3 circlePoint = radius * dirCircle;
-
-	return base + circlePoint;
 }
 
 glm::vec3 CylinderToCylinderContact::supportB(const ConvexInfo &cylinder, glm::vec3 dir)
@@ -52,54 +75,77 @@ glm::vec3 CylinderToCylinderContact::supportB(const ConvexInfo &cylinder, glm::v
 	glm::vec3 center = cylinder.center;				   // 중심점
 	glm::vec3 axis = glm::normalize(cylinder.axes[0]); // 높이 축 (단위 벡터)
 	float height = cylinder.height;
-	float radius = cylinder.radius;
 
 	// 1. 축 방향으로 윗면/아랫면 선택
-	glm::vec3 base = center;
 
-	if (glm::dot(dir, axis) > 0.0f)
+	bool isUpSide = true;
+	float dotResult = glm::dot(dir, axis);
+
+	if (dotResult < 0)
 	{
-		base += 0.5f * height * axis;
+		isUpSide = false;
+	}
+
+	glm::vec3 circleDir = dir - glm::dot(dir, axis) * axis; // 축에 수직한 방향
+
+	if (glm::length2(circleDir) > 1e-8f)
+	{
+		circleDir = glm::normalize(circleDir); // 정규화
+		
+		int32_t maxIdx;
+		int32_t segments = 20;
+
+		float max = -FLT_MAX;
+		for (int32_t i = 0; i < segments; ++i)
+		{
+			dotResult = glm::dot(cylinder.points[i], dir);
+			if (dotResult > max)
+			{
+				maxIdx = i;
+				max = dotResult;
+			}
+		}
+
+		if (isUpSide == false)
+		{
+			maxIdx += segments;
+		}
+
+		return cylinder.points[maxIdx];
 	}
 	else
 	{
-		base += -0.5f * height * axis;
+		if (isUpSide)
+		{
+			return center + cylinder.axes[0] * height * 0.5f;
+		}
+		else
+		{
+			return center - cylinder.axes[0] * height * 0.5f;
+		}
 	}
-
-	// 2. 원형 단면의 가장 먼 점 계산
-	glm::vec3 dirCircle = dir - glm::dot(dir, axis) * axis; // 축에 수직한 방향
-	if (glm::length(dirCircle) > 0.0001f)
-	{
-		dirCircle = glm::normalize(dirCircle); // 정규화
-	}
-	else
-	{
-		dirCircle = glm::vec3(0.0f, 0.0f, 0.0f); // 축 방향으로만 정렬된 경우
-	}
-	glm::vec3 circlePoint = radius * dirCircle;
-
-	return base + circlePoint;
 }
 
 void CylinderToCylinderContact::findCollisionPoints(const ConvexInfo &cylinderA, const ConvexInfo &cylinderB,
-												  std::vector<CollisionInfo> &collisionInfoVector, EpaInfo &epaInfo,
-												  std::vector<Simplex> &simplexVector)
+													std::vector<CollisionInfo> &collisionInfoVector, EpaInfo &epaInfo,
+													std::vector<Simplex> &simplexVector)
 {
-	std::cout << "cylinder vs cylinder!!\n";
+	// std::cout << "cylinder vs cylinder!!\n";
 	// clipping
 	Face refFace = getCylinderFace(cylinderA, epaInfo.normal);
 	Face incFace = getCylinderFace(cylinderB, -epaInfo.normal);
 
-	for (int i = 0; i < refFace.vertices.size(); i++)
-	{
-		std::cout << "refFace[" << i << "]: " << refFace.vertices[i].x << " " << refFace.vertices[i].y << " " << refFace.vertices[i].z << "\n";
-	}
+	// for (int i = 0; i < refFace.vertices.size(); i++)
+	// {
+	// 	std::cout << "refFace[" << i << "]: (" << refFace.vertices[i].x << ", " << refFace.vertices[i].y << ", " <<
+	// refFace.vertices[i].z << ")\n";
+	// }
 
-
-	for (int i = 0; i < incFace.vertices.size(); i++)
-	{
-		std::cout << "incFace[" << i << "]: " << incFace.vertices[i].x << " " << incFace.vertices[i].y << " " << incFace.vertices[i].z << "\n";
-	}
+	// for (int i = 0; i < incFace.vertices.size(); i++)
+	// {
+	// 	std::cout << "incFace[" << i << "]: (" << incFace.vertices[i].x << ", " << incFace.vertices[i].y << ", " <<
+	// incFace.vertices[i].z << ")\n";
+	// }
 
 	std::vector<glm::vec3> contactPolygon = computeContactPolygon(refFace, incFace);
 
