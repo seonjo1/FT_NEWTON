fbdf354c3a59eb4850085851e669ba9299da402c
seonjo1 <tjdgus9684@gmail.com>
2025-01-16 20:25:38 +0900

    [Refactoring]: Update code to adhere to coding conventions in SphereShape

--- include/physics/SphereShape.h
+++ include/physics/SphereShape.h
@@ -15,16 +15,10 @@ class SphereShape : public Shape
 	SphereShape *clone() const;
 	int32_t getChildCount() const;
 	void computeAABB(AABB *aabb, const Transform &xf) const;
-	void computeMass(MassData *massData, float density) const;
-	void setCenter(const glm::vec3 &center);
-	void setRadius(float radius);
-	void setShapeFeatures(std::vector<Vertex>& vertices);
-	virtual float getLocalRadius() const override;
-	virtual const glm::vec3& getLocalHalfSize() const override;
+	void setShapeFeatures(std::vector<Vertex> &vertices);
 	virtual ConvexInfo getShapeInfo(const Transform &transform) const override;
 
 	float m_radius;
-	float localRadius;
 };
 } // namespace ale
 #endif
