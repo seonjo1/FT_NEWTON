cf9901400902ac718b89ddc0810ee38ffdda3171
seonjo1 <tjdgus9684@gmail.com>
2025-01-16 20:25:29 +0900

    [Refactoring]: Update code to adhere to coding conventions in ShapeCollision

--- include/physics/ShapeCollisions.h
+++ include/physics/ShapeCollisions.h
@@ -1,19 +1,18 @@
 #ifndef SHAPECOLLISIONS_H
 #define SHAPECOLLISIONS_H
 
-#include "physics/SphereToSphereContact.h"
 #include "physics/SphereToBoxContact.h"
-#include "physics/SphereToCylinderContact.h"
 #include "physics/SphereToCapsuleContact.h"
+#include "physics/SphereToCylinderContact.h"
+#include "physics/SphereToSphereContact.h"
 
 #include "physics/BoxToBoxContact.h"
-#include "physics/BoxToCylinderContact.h"
 #include "physics/BoxToCapsuleContact.h"
+#include "physics/BoxToCylinderContact.h"
 
-#include "physics/CylinderToCylinderContact.h"
 #include "physics/CylinderToCapsuleContact.h"
+#include "physics/CylinderToCylinderContact.h"
 
 #include "physics/CapsuleToCapsuleContact.h"
-
 
 #endif
