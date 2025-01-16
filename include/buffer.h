6a9129e82f6ccfb0f242d77a7573cc50f70e6008
seonjo1 <tjdgus9684@gmail.com>
2025-01-16 20:18:52 +0900

    [Refactoring]: Update code to adhere to coding conventions in Buffer

--- include/buffer.h
+++ include/buffer.h
@@ -1,27 +1,29 @@
 #ifndef BUFFER_H
-# define BUFFER_H
+#define BUFFER_H
 
-# include "common.h"
-# include "deviceManager.h"
-# include "vertex.h"
+#include "Common.h"
+#include "DeviceManager.h"
+#include "Vertex.h"
 
-# include <chrono>
+#include <chrono>
 
 struct Vertex;
 
 class Buffer
 {
-public:
+  public:
 	virtual ~Buffer() = default;
 	void clear();
 	VkBuffer getBuffer();
 	VkDeviceMemory getBufferMemory();
-	std::vector<VkBuffer>& getBuffers();
+	std::vector<VkBuffer> &getBuffers();
 
-protected:
+  protected:
 	Buffer() = default;
-	void createBuffer(VkPhysicalDevice physicalDevice, VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
-	void copyBuffer(VkQueue graphicsQueue, VkCommandPool commandPool, VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
+	void createBuffer(VkPhysicalDevice physicalDevice, VkDeviceSize size, VkBufferUsageFlags usage,
+					  VkMemoryPropertyFlags properties, VkBuffer &buffer, VkDeviceMemory &bufferMemory);
+	void copyBuffer(VkQueue graphicsQueue, VkCommandPool commandPool, VkBuffer srcBuffer, VkBuffer dstBuffer,
+					VkDeviceSize size);
 	void endSingleTimeCommands(VkQueue graphicsQueue, VkCommandPool commandPool, VkCommandBuffer commandBuffer);
 	uint32_t findMemoryType(VkPhysicalDevice physicalDevice, uint32_t typeFilter, VkMemoryPropertyFlags properties);
 	VkCommandBuffer beginSingleTimeCommands(VkCommandPool commandPool);
@@ -32,57 +34,57 @@ protected:
 	uint32_t size;
 };
 
-
 class VertexBuffer : public Buffer
 {
-public:
-	static std::unique_ptr<VertexBuffer> create(std::vector<Vertex> vertices, DeviceManager* deviceManager, VkCommandPool commandPool);
+  public:
+	static std::unique_ptr<VertexBuffer> create(std::vector<Vertex> vertices, DeviceManager *deviceManager,
+												VkCommandPool commandPool);
 	virtual ~VertexBuffer() = default;
 	uint32_t getVerticesSize();
 
-private:
+  private:
 	VertexBuffer() = default;
-	void init(std::vector<Vertex> vertices, DeviceManager* deviceManager, VkCommandPool commandPool);
+	void init(std::vector<Vertex> vertices, DeviceManager *deviceManager, VkCommandPool commandPool);
 	uint32_t vSize;
 };
 
 class IndexBuffer : public Buffer
 {
-public:
-	static std::unique_ptr<IndexBuffer> create(std::vector<uint32_t> indices, DeviceManager* deviceManager, VkCommandPool commandPool);
+  public:
+	static std::unique_ptr<IndexBuffer> create(std::vector<uint32_t> indices, DeviceManager *deviceManager,
+											   VkCommandPool commandPool);
 	virtual ~IndexBuffer() = default;
 	uint32_t getIndicesSize();
-	
-private:
+
+  private:
 	IndexBuffer() = default;
-	void init(std::vector<uint32_t> indices, DeviceManager* deviceManager, VkCommandPool commandPool);
+	void init(std::vector<uint32_t> indices, DeviceManager *deviceManager, VkCommandPool commandPool);
 	uint32_t iSize;
 };
 
-
 class UniformBuffer : public Buffer
 {
-public:
-	static std::unique_ptr<UniformBuffer> create(DeviceManager* deviceManager, VkDeviceSize bufferSize);
-	void update(UniformBufferObject& ubo, uint32_t currentFrame);
+  public:
+	static std::unique_ptr<UniformBuffer> create(DeviceManager *deviceManager, VkDeviceSize bufferSize);
+	void update(UniformBufferObject &ubo, uint32_t currentFrame);
 	virtual ~UniformBuffer() = default;
 
-private:
+  private:
 	UniformBuffer() = default;
-	void init(DeviceManager* deviceManager, VkDeviceSize bufferSize);
+	void init(DeviceManager *deviceManager, VkDeviceSize bufferSize);
 
-	std::vector<void*> buffersMapped;
+	std::vector<void *> buffersMapped;
 };
 
 class StagingBuffer : public Buffer
 {
-public:
+  public:
 	virtual ~StagingBuffer() = default;
-	static std::unique_ptr<StagingBuffer> create(DeviceManager* deviceManager, VkDeviceSize imageSize);
-	
-private:
+	static std::unique_ptr<StagingBuffer> create(DeviceManager *deviceManager, VkDeviceSize imageSize);
+
+  private:
 	StagingBuffer() = default;
-	void init(DeviceManager* deviceManager, VkDeviceSize imageSize);
+	void init(DeviceManager *deviceManager, VkDeviceSize imageSize);
 };
 
 #endif
