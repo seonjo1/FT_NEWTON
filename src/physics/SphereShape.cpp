cec7629486736ab266d7cee00775fbead8706452
seonjo1 <tjdgus9684@gmail.com>
2025-01-16 20:25:06 +0900

    [Refactoring]: Update code to adhere to coding conventions in SphereShape

--- src/physics/SphereShape.cpp
+++ src/physics/SphereShape.cpp
@@ -5,7 +5,7 @@ namespace ale
 {
 SphereShape::SphereShape()
 {
-	m_type = Type::SPHERE;
+	m_type = EType::SPHERE;
 }
 
 SphereShape *SphereShape::clone() const
@@ -32,24 +32,11 @@ void SphereShape::computeAABB(AABB *aabb, const Transform &xf) const
 	aabb->upperBound = upper + glm::vec3(0.1f);
 	aabb->lowerBound = lower - glm::vec3(0.1f);
 }
-void SphereShape::computeMass(MassData *massData, float density) const
-{
-}
-
-void SphereShape::setCenter(const glm::vec3 &center)
-{
-	m_center = center;
-}
-
-void SphereShape::setRadius(float radius)
-{
-	m_radius = radius;
-}
 
 void SphereShape::setShapeFeatures(std::vector<Vertex> &vertices)
 {
 	// welzl 알고리즘 나중에 적용 고려
-	localCenter = glm::vec3(0.0f);
+	m_center = glm::vec3(0.0f);
 	float distance = 0.0f;
 
 	for (const Vertex &vertex : vertices)
@@ -59,24 +46,14 @@ void SphereShape::setShapeFeatures(std::vector<Vertex> &vertices)
 							distance);
 	}
 
-	localRadius = std::sqrt(distance);
-}
-
-float SphereShape::getLocalRadius() const
-{
-	return localRadius;
-}
-
-const glm::vec3 &SphereShape::getLocalHalfSize() const
-{
-	return glm::vec3(0.0f);
+	m_radius = std::sqrt(distance);
 }
 
 ConvexInfo SphereShape::getShapeInfo(const Transform &transform) const
 {
 	ConvexInfo sphere;
-	sphere.radius = localRadius;
-	sphere.center = transform.toMatrix() * glm::vec4(localCenter, 1.0f);
+	sphere.radius = m_radius;
+	sphere.center = transform.toMatrix() * glm::vec4(m_center, 1.0f);
 	return sphere;
 }
 
