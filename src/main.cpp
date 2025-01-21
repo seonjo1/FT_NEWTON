808de161a70d0e779493760b7e8bbdeb010accea
seonjo1 <tjdgus9684@gmail.com>
2025-01-21 17:21:00 +0900

    [Feat]: add leak check in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
+#define _CRTDBG_MAP_ALLOC
 #include "../include/App.h"
+#include <crtdbg.h>
 
 int main()
 {
-	App app;
 
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	App app;
+	
 	try
 	{
 		app.run();
